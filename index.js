73ccdec8bb318f705bc4f0362b00431da00b7c9f
Terry Kwon <terrykkwon@gmail.com>
2018-02-03 00:26:25 +0900

    Apply bare styles

--- index.js
+++ index.js
@@ -9,30 +9,33 @@ function changeDeltaText(delta) {
     if (delta <= 0) {
         resStr = "아쉽지만 군복무가 단축되지 않았습니다.";
     } else {
-        resStr = "축하합니다! 군복무가 " + delta + "일 단축되었습니다.";
+        resStr = "축하합니다! 군복무가 <span class='accented'>" + delta + "일</span> 단축되었습니다.";
     }
 
-    $('.results--delta').text(resStr);
+    $('.results--delta').empty().append(resStr);
 }
 
 function changeEndDateText(endDate) {
-    let dateStr = "전역일은 " + endDate.year() + "년 "
+    let dateStr = "전역일은 <span class='accented'>" + endDate.year() + "년 "
         + (endDate.month() + 1) + "월 "
-        + endDate.date() + "일 "
+        + endDate.date() + "일</span> "
         + "입니다.";
 
-    $('.results--date').text(dateStr);
+    $('.results--date').empty().append(dateStr);
 }
 
 
 $(document).ready(function() {
 
     $("#submit").click(function() {
-        $('.results').show();
-
         let joinDateString = $('#join_date').val();
-
         let joinDate = moment(joinDateString, 'YYMMDD');
+        if (!joinDate.isValid()) {
+            alert('날짜를 올바르게 입력하여 주세요 (예. 170101)');
+            return;
+        }
+
+        $('.results').show();
 
         let referenceDate = moment("161004", 'YYMMDD'); // Date when policy goes in effect
 
@@ -45,12 +48,12 @@ $(document).ready(function() {
 
         prevEndDate = moment(joinDate);
         if (serviceType === 'airforce') {
-            prevEndDate.year(prevEndDate.years() + 2);
+            prevEndDate.year(prevEndDate.year() + 2);
         } else if (serviceType === 'navy') {
             prevEndDate = moment(joinDate);
-            prevEndDate.month(prevEndDate.months() + 23);
+            prevEndDate.month(prevEndDate.month() + 23);
         } else {
-            prevEndDate.month(prevEndDate.months() + 21);
+            prevEndDate.month(prevEndDate.month() + 21);
         }
 
         changeDeltaText(reducedDays);
